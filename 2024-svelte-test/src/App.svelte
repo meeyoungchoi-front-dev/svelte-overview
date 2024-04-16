baf8052ec5f5002389e768696ab8ec5a958feaf7
meeyoungchoi <163703903+meeyoungchoi-front-dev@users.noreply.github.com>
2024-04-16 16:50:02 +0900

    svelte api를 활용하여 click event 처리하기

--- 2024-svelte-test/src/App.svelte
+++ 2024-svelte-test/src/App.svelte
@@ -1,22 +1,22 @@
 <script>
-  import { onMount } from 'svelte'//현재 컴포넌트가 준비되면 콜백 함수의 내용을 실행하겠다
+  //import { onMount } from 'svelte'//현재 컴포넌트가 준비되면 콜백 함수의 내용을 실행하겠다
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
   let name = 'world';
   let isRed = false;
-  onMount(() => {
-    const box = document.querySelector(".box");
-    box.addEventListener('click', () => {
-      isRed = !isRed;
-    })
-  })
+  // onMount(() => {
+  //   const box = document.querySelector(".box");
+  //   box.addEventListener('click', () => {
+  //     isRed = !isRed;
+  //   })
+  // })
   
 </script>
 
 <main>
 <h1>Hello {name}</h1>
-<div class="box" style="background-color: {isRed ? 'red' : 'orange'}">Box!</div>
+<div class="box" style="background-color: {isRed ? 'red' : 'orange'};" on:click={() => {isRed = !isRed}}>Box!</div>
 </main>
 
 <style>
