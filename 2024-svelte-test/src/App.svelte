1ec5bf6ec6849176e676b9b55b67faf11c51dfd4
meeyoungchoi <163703903+meeyoungchoi-front-dev@users.noreply.github.com>
2024-04-16 16:24:32 +0900

    svelte - if문

--- 2024-svelte-test/src/App.svelte
+++ 2024-svelte-test/src/App.svelte
@@ -3,23 +3,24 @@
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
   let name = 'world';
-  let age = 85;
-  function assign() {
-    name = "코딩묭이";
-    age = 29;
+  let toggle = false;
+  if (toggle) {
+
+  } else {
+
   }
 </script>
 
 <main>
-<h1>Hello {name}</h1>
-<h2 class={age < 85 ? "active": ""}> <!--중괄호를 이용한 선언적 랜더링을 사용할 수 있다-->
-  {age}
-</h2>
-<img src="" alt={name}/>
-<input type="text" bind:value={name}/> <!-- bind를 활용하여 name 필드에 데이터를 바인딩해줄수 있다-->
-<button on:click={assign}>
-  Submit
-</button>
+
+<button on:click={() => {toggle = !toggle}}>Toggle</button>
+{#if toggle}
+  <h1>Hello {name}</h1>
+{:else}
+  <div>
+    No Name!
+  </div>
+{/if}
 </main>
 
 <style>
