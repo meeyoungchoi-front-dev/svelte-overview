613336288c8414932266f5c3c81831e30fdcc25d
meeyoungchoi <163703903+meeyoungchoi-front-dev@users.noreply.github.com>
2024-04-17 09:41:07 +0900

    svelte - store를 사용하여 특정 컴포넌트에서만 액세스 애햐 하는 값을 처리

--- 2024-svelte-test/src/App.svelte
+++ 2024-svelte-test/src/App.svelte
@@ -2,10 +2,16 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte' 
+  import { storeName } from './store.js';
   import Fruits from './components/Fruits.svelte';
+  import Parent from './components/Parent.svelte';
+  import Child from './components/Child.svelte';
   let fruits = ['Apple','Banana','Cherry','Orange','Mango'];
+  let word = 'world';
+  $storeName = word
 </script>
 <Fruits fruits={fruits}/>
 <Fruits fruits={fruits} reverse={true}/>
 <Fruits fruits={fruits} slice="-2"/>
 <Fruits fruits={fruits} slice="0, 3"/>
+<Parent/>
